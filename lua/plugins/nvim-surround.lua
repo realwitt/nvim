2cf58e85a5cc0cbcca2ab123a1ca9a1586748216
Elias Witt <ew@eliaswitt.com>
2025-12-17 10:44:47 -0500

    fix nvim surround and add whichkey insights

--- lua/plugins/nvim-surround.lua
+++ lua/plugins/nvim-surround.lua
@@ -1,10 +1,31 @@
 return {
-  "kylechui/nvim-surround",
-  version = "^3.0.0", -- Use for stability; omit to use `main` branch for the latest features
-  event = "VeryLazy",
-  config = function()
-    require("nvim-surround").setup({
-      -- Configuration here, or leave empty to use defaults
-    })
-  end,
+  -- nvim-surround: Lua port of tpope's vim-surround
+  {
+    "kylechui/nvim-surround",
+    version = "^3.0.0",
+    event = "VeryLazy",
+    opts = {
+      keymaps = {
+        -- Use 's' in visual mode like classic vim-surround
+        visual = "s",
+        visual_line = "S",
+      },
+    },
+  },
+
+  -- Disable flash.nvim's 's' mapping in visual mode to avoid conflict
+  {
+    "folke/flash.nvim",
+    opts = {
+      modes = {
+        char = {
+          keys = { "f", "F", "t", "T", [";"] = "L", [","] = "H" },
+        },
+      },
+    },
+    keys = {
+      -- Remove the 's' visual mode mapping from flash
+      { "s", mode = { "n", "x", "o" }, false },
+    },
+  },
 }
---
+++ lua/plugins/which-key.lua
@@ -0,0 +1,20 @@
+return {
+  "folke/which-key.nvim",
+  opts = {
+    spec = {
+      -- Surround keymaps (normal mode)
+      { "ys", desc = "Surround: Add" },
+      { "ds", desc = "Surround: Delete" },
+      { "cs", desc = "Surround: Change" },
+
+      -- Surround keymaps (visual mode)
+      { "s", desc = "Surround selection", mode = "v" },
+      { "S", desc = "Surround lines", mode = "v" },
+
+      -- Picker window navigation
+      { "<C-h>", desc = "Window left / Focus list", mode = { "n", "i" } },
+      { "<C-l>", desc = "Window right / Focus preview", mode = { "n", "i" } },
+    },
+  },
+}
+
